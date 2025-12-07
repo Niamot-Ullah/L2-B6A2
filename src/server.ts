77b30539b0c8d5f20fa7336b73e54dd29113bf2c
Niamot Ullah <niamotullah009@gmail.com>
2025-12-07 19:22:47 +0600

    deployment and readme file

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,8 @@
-import express, { NextFunction, Request, Response } from "express";
+import app from "./app";
 import config from "./config";
-import initDB, { pool } from "./config/db";
-import logger from "./middleware/logger";
-import { userRoutes } from "./modules/users/users.routes";
-import { vehiclesRoutes } from "./modules/vehicles/vehicles.routes";
-import { bookingRoutes } from "./modules/bookings/booking.route";
-import { authRoutes } from "./modules/auth/auth.routes";
 
-const app = express();
+
 const port = config.port;
-app.use(express.json());
-
-//db
-initDB();
-
-
-// server -> route-> controller -> services
-
-//root route
-app.get("/",logger, (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "this is root",
-  });
-});
-
-//user crud
-app.use("/api/v1/users", userRoutes)
-
-// vehicle crud
-app.use("/api/v1/vehicles",vehiclesRoutes);
-
-//booking crud
-app.use("/api/v1/bookings", bookingRoutes);
-
-//auth route
-app.use("/api/v1/auth",authRoutes)
-
-
-//error route handler
-app.use((req,res)=>{
-    res.status(404).json({
-        success:false,
-        message:"Route not found",
-        path:req.path
-    })
-})
-
-
 
 
 app.listen(port, () => {
